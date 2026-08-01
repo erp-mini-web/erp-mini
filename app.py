3438685911a80833506336ec9cd6885830174cb6
erp-mini-web <n09046450797@gmail.com>
2026-08-01 20:21:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,3 @@
-#from flask import Flask
-
-#app = Flask(__name__)
-
-#@app.route("/")
-#def home():
-#    return "ERP-miniが動いている!"
-
-#if __name__ == "__main__":
-#   import os
-#    port = int(os.environ.get("PORT", 5000))
-#    app.run(host="0.0.0.0", port=port)
-
-
-#from flask import Flask, render_template
-
-#app1 = Flask(__name__)
-
-#@app1.route("/")
-#def menu():
-#    return render_template("menu.html")
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
