dd627f35f4b168e5746e7ffb745d04cda2c69c41
erp-mini-web <n09046450797@gmail.com>
2026-08-01 19:41:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "ERP-mini is running!"
+
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
